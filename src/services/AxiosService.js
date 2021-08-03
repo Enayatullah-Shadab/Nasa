ddccbeb843499958df7c7c57c3b67186b0f2f771
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-03 16:01:42 -0600

    Architectured

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,6 +1,6 @@
-import Axios from 'axios'
-import { baseURL } from '../env'
-export const api = Axios.create({
-  baseURL,
-  timeout: 8000
+import axios from 'axios'
+
+export const NasaApi = axios.create({
+  baseURL: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
+  timeout: 5000
 })
